f0f88929055864a5e6295431ba5b8c93ab867c63
Harshita Lalwani <134720361+harshita0212@users.noreply.github.com>
2025-07-21 19:53:43 +0545

    model trainer added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig)
+from mlProject.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
 
 
 
@@ -60,3 +60,38 @@ class ConfigurationManager:  # loads config.yaml
         )
 
         return data_transformation_config
+    
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
+        )
+
+        return model_trainer_config
+
+from mlProject.entity.config_entity import ModelTrainerConfig
+
+# test initialization
+dummy = ModelTrainerConfig(
+    root_dir="root",
+    train_data_path="train.csv",
+    test_data_path="test.csv",
+    model_name="model.joblib",
+    alpha=0.5,
+    l1_ratio=0.3,
+    target_column="target"
+)
+print(dummy)
